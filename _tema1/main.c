acf7763538c3c5ae76be094613c27b5803466930
F500 <magnumf500@gmail.com>
2021-05-07 18:21:36 -0500

    Tema1:HolaMundo

---
+++ _tema1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> /*Directiva Standard Input/Ouput */
+#include <conio.h>
+
+/*Programa Hola Mundo!*/
+//Esto tambien es un comentario.
+int main()
+{
+    printf("Hola Mundo! \n");
+    printf("Pulse una tecla para salir...");
+    getch(); /*Pausa*/
+    return 0;
+}
